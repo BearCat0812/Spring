2075169295582df1816b10653fe0a7baf4d8cda5
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-06-20 10:07:39 +0900

    25.06.20

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.model.UserDTO;
+import com.example.service.UserService;
 
 @Controller
 public class HomeController {
@@ -21,5 +26,15 @@ public class HomeController {
 	@GetMapping("/TestJsp")
 	public String Jsp() {
 		return "Test";
+	}
+	
+	@Autowired
+	private UserService userService; // 의존성 주입 - 필드주입
+	
+	@GetMapping("/Test")
+	public String Test(Model model) {
+		List<UserDTO> users = userService.getAllUser();
+		model.addAttribute("users",users);
+		return "DBTest";
 	}
 }
